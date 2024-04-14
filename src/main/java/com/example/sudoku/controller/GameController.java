70918fd69bd5da44329cea54ce3d66ae1051be40
Nathal25 <nathalia.ortiz.granada@correounivalle.edu.co>
2024-04-14 17:25:37 -0500

    Se agregan numeros random del 1 al 9 en los textField

--- src/main/java/com/example/sudoku/controller/GameController.java
+++ src/main/java/com/example/sudoku/controller/GameController.java
@@ -1,10 +1,8 @@
 package com.example.sudoku.controller;
 
-import javafx.event.ActionEvent;
+
 import javafx.event.EventHandler;
-import javafx.event.EventType;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
@@ -13,21 +11,27 @@ public class GameController {
     //Test second commit
     @FXML
     private GridPane gridPaneSudoku;
+
+   private int numRandom;
     @FXML
     public void initialize(){
 
+        System.out.println("El numero actual es: "+numRandom);
         for (int i=0; i<9; i++){
             for (int j=0; j<9; j++){
+                numRandom = (int) (Math.random()*9+1);
                 TextField textField = new TextField();
+                textField.setEditable(false);
                 textField.setMaxWidth(37);
                 textField.setMaxHeight(37);
                 textField.setStyle("-fx-border-color: black;");
+                textField.setText(String.valueOf(numRandom));
                 gridPaneSudoku.add(textField, i, j);
-                textFielLetterGiven(textField, i, j);
+                textFieldLetterGiven(textField, i, j);
             }
         }
     }
-    private void textFielLetterGiven(TextField textField, int i, int j){
+    private void textFieldLetterGiven(TextField textField, int i, int j){
         textField.setOnKeyTyped(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent keyEvent) {
