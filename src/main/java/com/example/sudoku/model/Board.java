90a5c2e3f7fea48dcda707b674bbe8338a5f8209
Nathal25 <nathalia.ortiz.granada@correounivalle.edu.co>
2024-04-22 16:54:42 -0500

    Sea agregó un tablero mas al board y se optimizó la asignación del tablero respuesta

--- src/main/java/com/example/sudoku/model/Board.java
+++ src/main/java/com/example/sudoku/model/Board.java
@@ -32,6 +32,17 @@ public class Board {
                 {5,4,9,2,7,6,1,8,3},
                 {7,2,1,5,8,3,9,6,4}
         };
+        int sudokuComplete3[][]={
+                {1,9,2,3,7,6,5,4,8},
+                {4,5,6,9,8,1,7,3,2},
+                {7,3,8,2,5,4,9,6,1},
+                {3,1,7,8,6,9,4,2,5},
+                {6,2,5,4,1,7,3,8,9},
+                {8,4,9,5,2,3,1,7,6},
+                {9,7,1,6,3,2,8,5,4},
+                {2,8,4,7,9,5,6,1,3},
+                {5,6,3,1,6,8,2,9,7}
+        };
 
         int sudokuIncomplete1[][]={
             {0,6,0,3,0,0,0,0,0},
@@ -44,7 +55,7 @@ public class Board {
             {4,7,2,0,0,5,6,0,9},
             {0,0,0,9,2,0,7,0,5}
         };
-        int sudokuImcomplete2[][]={
+        int sudokuIncomplete2[][]={
                 {0,0,4,0,0,7,0,1,0},
                 {1,0,0,8,2,0,3,4,0},
                 {0,0,0,1,9,0,0,0,0},
@@ -55,19 +66,32 @@ public class Board {
                 {5,0,9,0,7,0,0,8,3},
                 {0,0,1,0,8,3,0,0,4}
         };
+        int sudokuIncomplete3[][]={
+                {0,9,0,0,0,0,0,0,0},
+                {0,0,6,0,0,1,7,3,0},
+                {7,0,0,0,0,0,0,6,0},
+                {3,1,7,0,0,9,0,0,0},
+                {0,0,5,4,0,7,3,0,0},
+                {0,0,9,5,2,0,1,7,6},
+                {9,0,1,0,3,2,8,5,0},
+                {2,0,4,7,0,5,0,0,3},
+                {0,0,0,0,0,0,0,9,0}
+        };
         ArrayList<int[][]> sudokusIncompletesAvailables=new ArrayList<>();
-        sudokusIncompletesAvailables.add(sudokuImcomplete2);
         sudokusIncompletesAvailables.add(sudokuIncomplete1);
+        sudokusIncompletesAvailables.add(sudokuIncomplete2);
+        sudokusIncompletesAvailables.add(sudokuIncomplete3);
+
+        ArrayList<int[][]> sudokusCompletesAvailables=new ArrayList<>();
+        sudokusCompletesAvailables.add(sudokuComplete1);
+        sudokusCompletesAvailables.add(sudokuComplete2);
+        sudokusCompletesAvailables.add(sudokuComplete3);
 
         //Permite que se genere alguno de los sudokus preparados
-        int sudokuAleatorio=(int)(Math.random()*2);
-        boardIncomplete=sudokusIncompletesAvailables.get(sudokuAleatorio);
-        if(boardIncomplete==sudokuIncomplete1){
-            boardSolution=sudokuComplete1;
-        }
-        else {
-            boardSolution=sudokuComplete2;
-        }
+        int sudokuAleatorio=(int)(Math.random()*sudokusIncompletesAvailables.size());
+        boardIncomplete=sudokusIncompletesAvailables.get(sudokuAleatorio);  //se selecciona un sudoku de la lista en base al numero aleatorio
+        boardSolution=sudokusCompletesAvailables.get(sudokuAleatorio);      //Se selecciona el sudoku respuesta con el mismo numero que el anterior
+
     }
     public int[][] getBoardSolution() {return boardSolution;}
     public int[][] getBoardIncomplete() {return boardIncomplete;}
